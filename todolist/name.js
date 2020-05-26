da7da15bebc1037c18eab47309eeccb6d0d88ff2
csjv95 <csjv95@gmail.com>
2020-05-26 21:32:16 +0900

    docs : add function saveName. for remember user name

--- todolist/name.js
+++ todolist/name.js
@@ -11,11 +11,16 @@ function paintName(text) {
   name.innerText = `Hello ${text}`;
 }
 
+function saveName(text) {
+  localStorage.setItem(USER_LS,text);
+}
+
 function handleSubmit(event) {
   const currentValue = input.value; 
 
   event.preventDefault(); //submit 이벤트가 발생할때 default한다
-  paintName(currentValue); //submit 이벤트 발생 하였을때 input.value를 pait한다
+  paintName(currentValue); //submit 이벤트 발생 하였을때 input.value를 paint한다
+  saveName(currentValue); //localStorage에 저장한다 input.value를
 } //submit 관리
 
 function askForName() {
@@ -25,7 +30,7 @@ function askForName() {
 } //스토로지에서 currentUser가 없을때 SHOWING_CN 추가 하여 물어보기
 
 
-function localStorge() {
+function niceMeet() {
   const currentUser = localStorage.getItem(USER_LS);
 
   if(currentUser === null) {
@@ -36,6 +41,6 @@ function localStorge() {
 } // 로컬 스트로지에서 값을 받아오기
 
 function init() {
-  localStorge();
+  niceMeet();
 }
 init();

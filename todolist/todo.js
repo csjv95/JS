fae74f5101f68bebe9fa7b36aeef5e9bd1068107
csjv95 <csjv95@gmail.com>
2020-06-01 10:35:19 +0900

    docs : add function and parsing and up load list

--- todolist/todo.js
+++ todolist/todo.js
@@ -4,34 +4,59 @@ const toDoList = document.querySelector('.js-toDoList');
 
 const TODOS_LS = 'toDos';
 
+const toDos = []; // to dos
+
+// save toDos
+function saveToDos() {
+  localStorage.setItem(TODOS_LS, JSON.stringify(toDos)); // setItem(keyName , keyValue),JSON을 사용해서 object를 string으로 바꿔준다
+}
+
+ // paint todo
 function paintToDo(text) {
-  const li = document.createElement('li');
-  const span = document.createElement('span');
-  const delBtn = document.createElement('button');
-  
+  const li = document.createElement('li'); // <li> 생성
+  const span = document.createElement('span'); // <span> 생성
+  const delBtn = document.createElement('button'); // 삭제 버튼
+  const newId = toDos.length + 1; // id값은 length에 +1
+
   delBtn.innerText ='x';
   span.innerText = text;
   
   li.appendChild(span);
   li.appendChild(delBtn);
-  toDoList.appendChild(li);
-}// paint todo
+  li.id = newId;
 
+  toDoList.appendChild(li);
+
+  const toDoObj = {
+    text : text,
+    id: newId
+  };
+
+  toDos.push(toDoObj);  // toDos에 toDoObj를 push한다
+  saveToDos();
+}
+
+//submit 관리
 function handleSubmit(event) {
   const currentValue = toDoInput.value;
 
   event.preventDefault();
   paintToDo(currentValue);
   toDoInput.value ='';
-} //submit 관리
+}
 
+
+//toDolist를 load하는것
 function loadToDos() {
-  const toDos = localStorage.getItem(TODOS_LS);
+  const loadedToDos = localStorage.getItem(TODOS_LS);
   
-  if(toDos !== null) {
-    //있으면
+  if(loadedToDos !== null) {
+    const parsedToDos = JSON.parse(loadedToDos); // JSON을 사용하여서 localStroge에있는 문자열을 parsing 해서 obj
+    parsedToDos.forEach(function(toDo) {
+      paintToDo(toDo.text);
+    });
   }
-} //toDolist를 load하는것
+}
 
 function init() {
   loadToDos();

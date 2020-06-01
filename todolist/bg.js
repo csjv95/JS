b0556222eb17b1e12bf6b480b21b2e1cbbc1402b
csjv95 <csjv95@gmail.com>
2020-06-01 16:18:03 +0900

    docs : add some images and add funcion chage bg img

---
+++ todolist/bg.js
@@ -0,0 +1,25 @@
+const body = document.querySelector('body');
+
+const IMG_NUMBER = 3;
+
+// paint Images
+function paintImage(imgNumber) {
+  const image = new Image();
+  
+  image.src = `image/${imgNumber + 1}.jpg`;
+  image.classList.add('bgImage');
+  body.appendChild(image);
+}
+
+// random number
+function genRandom() {
+  const number = Math.floor(Math.random()* IMG_NUMBER);
+  return number;
+}
+
+function init() {
+  const randomNumber = genRandom();
+  paintImage(randomNumber);
+}
+
+init();
